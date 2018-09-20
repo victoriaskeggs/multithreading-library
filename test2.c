18c0fb7ee724beebc84ec7eb97a1c2254a5d2a7d
Victoria Skeggs <victoria.skeggs@gmail.com>
2018-09-20 18:37:16 +1200

    Revert "Commented out print statements"
    This reverts commit 7376ef522c15a615be21ec437d06f956162c3f3b.
    
    Revert changes

--- test2.c
+++ test2.c
@@ -22,10 +22,17 @@ int main(int argc, char** argv) {
     // create a concurrent dispatch queue
     dispatch_queue_t * concurrent_dispatch_queue;
     task_t *task;
+	printf("Creating a dispatch queue.\n");
     concurrent_dispatch_queue = dispatch_queue_create(CONCURRENT);
+	printf("Dispatch queue created.\n");
+	printf("Creating a task.\n");
     task = task_create(test2, NULL, "test2");
+	printf("Task created.\n");
+	printf("Dispatching async.\n");
     dispatch_async(concurrent_dispatch_queue, task);
     printf("Safely dispatched\n");
+	printf("Destroying the queue.\n");
     dispatch_queue_destroy(concurrent_dispatch_queue);
+	printf("Queue destroyed.\n");
     return EXIT_SUCCESS;
 }
